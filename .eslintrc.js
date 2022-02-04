5f11ef35423a88281ad825207e13c317fd1ca605
smth324 <Isaerwilliamvicsongocheco@gmail.com>
2022-02-04 18:16:25 +0800

    fix errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   "env": {
     "browser": true,
-    "es2021": true,
+    "es6": true,
     "jest/globals": true,
     "node": true
   },
@@ -26,7 +26,7 @@ module.exports = {
       ],
     "linebreak-style": [
       "error",
-      "unix"
+      "windows"
     ],
     "quotes": [
       "error",
