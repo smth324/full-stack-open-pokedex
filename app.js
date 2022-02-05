eebee274fef9128a4ff73bb7ed5830f726ff1caa
smth324 <Isaerwilliamvicsongocheco@gmail.com>
2022-02-05 18:07:17 +0800

    changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('dist'))
 app.get('/health', (_req, res) => {
   res.send('ok')
 })
-
+// changes
 app.get('/version', (_req, res) => {
   res.send('2') // change this string to ensure a new version deployed
 })
